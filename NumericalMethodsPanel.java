9016348c83c816d4e4b0ff2c1e7bb3a45f1f1c2e
nataly-8h <54066974+nataly-8h@users.noreply.github.com>
2019-11-27 12:59:23 -0600

    78
    6

--- NumericalMethodsPanel.java
+++ NumericalMethodsPanel.java
@@ -1,61 +1,117 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.TextField;
-import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class NumericalMethodsPanel extends JPanel {
-	private double idealGasConstant  = 0.082054;
-	private TextField a,
-					  b;
-	
+import javax.swing.*;
+
+public class NumericalMethodsPanel extends JPanel implements ActionListener {
+	private double idealGasConstant = 0.082054;
+	private Image imagen;
+	private JButton solve;
+
 	public NumericalMethodsPanel() {
 		super();
 		this.setPreferredSize(new Dimension(400, 600));
 		this.setOpaque(true);
 		this.setBackground(Color.GRAY);
-		this.a = new TextField(12);
-		this.b = new TextField(12);
-		this.add(a);
-		this.add(b);
+
+		this.solve = new JButton("RESOLVER");
+		this.solve.addActionListener(this);
+		this.add(solve, BorderLayout.SOUTH);
+
+		this.imagen = new ImageIcon("Metodos.png").getImage();
 	}
-	
+
+	public void paint(Graphics g) {
+		g.drawImage(imagen, 0, 0, this);
+	}
+
 	public void NewtonRhapsonSolver(double a, double b, double temperature, double pressure) {
 		double error = 100;
 		double initialGuess = this.idealGasSolver(pressure, temperature);
 		System.out.println("The initial guess is " + initialGuess);
 		int count = 0;
-		while(error>0.001) {
+		double guess = 0;
+		while (error > 0.001) {
 			count++;
-			double newtonRhapsonGuess = initialGuess - (this.vanDerWaalsEquation(a, b, initialGuess, temperature, pressure)/this.vanDerWaalsEquationDerivative(a, b, initialGuess, temperature, pressure));
-			error = Math.abs(((newtonRhapsonGuess - initialGuess)/newtonRhapsonGuess));
-			System.out.println("The error is " + error);
+			double newtonRhapsonGuess = initialGuess
+					- (this.vanDerWaalsEquation(a, b, initialGuess, temperature, pressure)
+							/ this.vanDerWaalsEquationDerivative(a, b, initialGuess, temperature, pressure));
+			error = Math.abs(((newtonRhapsonGuess - initialGuess) / newtonRhapsonGuess));
+			// System.out.println("The error is " + error);
 			initialGuess = newtonRhapsonGuess;
-			System.out.println("The new guess is " + newtonRhapsonGuess);
-			System.out.println(count);
+			guess = newtonRhapsonGuess;
 		}
-		
-		
+
+		System.out.println("The new guess is " + guess);
+		System.out.println("Solved in : " + count + " iterations");
+		System.out.println(" ");
+
 	}
-	
+
 	public double idealGasSolver(double pressure, double temperature) {
-		return ((this.idealGasConstant*temperature)/pressure);
+		return ((this.idealGasConstant * temperature) / pressure);
 	}
-	
+
 	public double vanDerWaalsEquation(double a, double b, double v, double temperature, double pressure) {
-		double firstPart = pressure + (a/(Math.pow(v, 2)));
-		double secondPart = v-b;
-		return (firstPart * secondPart) - (this.idealGasConstant*temperature);
+		double firstPart = pressure + (a / (Math.pow(v, 2)));
+		double secondPart = v - b;
+		return (firstPart * secondPart) - (this.idealGasConstant * temperature);
 	}
-	
+
 	public double vanDerWaalsEquationDerivative(double a, double b, double v, double temperature, double pressure) {
-		double secondPart = a/(Math.pow(v, 2));
-		double thirdPart = (2*a*b)/(Math.pow(v, 3));
+		double secondPart = a / (Math.pow(v, 2));
+		double thirdPart = (2 * a * b) / (Math.pow(v, 3));
 		return pressure - secondPart + thirdPart;
 	}
+
 	public static void main(String[] args) {
-		NumericalMethodsPanel nm = new NumericalMethodsPanel();
-		nm.NewtonRhapsonSolver(3.592, 0.04267, 300, 1);
+//		NumericalMethodsPanel nm = new NumericalMethodsPanel();
+//		nm.NewtonRhapsonSolver(3.592, 0.04267, 300, 1);
+	}
+
+	public void leer() {
+
+		JFileChooser fc = new JFileChooser();
+		fc.showOpenDialog(this);
+		int respArchivo = fc.showOpenDialog(this);
+		if (respArchivo == JFileChooser.APPROVE_OPTION) {
+			File archivo = fc.getSelectedFile();
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(archivo.getAbsolutePath()));
+				String linea;
+				while ((linea = br.readLine()) != null) {
+
+					String[] line = linea.split(" ");
+					double[] lin = new double[line.length];
+					for (int i = 0; i < line.length; i++) {
+						lin[i] = Double.parseDouble(line[i]);
+					}
+					NewtonRhapsonSolver(lin[0], lin[1], lin[2], lin[3]);
+				}
+				br.close();
+			} catch (IOException ex) {
+				System.out.println("ERROR");
+			}
+		}
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == solve) {
+			leer();
+			System.out.println("wuwiuwi");
+		}
 	}
 }
-
-	
